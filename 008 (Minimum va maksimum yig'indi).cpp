8aa9065cfc622a4e62c943753807b5df2b6c7a20
Fazliddin <149813353+fazliddindev1@users.noreply.github.com>
2024-09-02 20:28:42 +0500

    Update 008 (Minimum va maksimum yig'indi).cpp

--- 008 (Minimum va maksimum yig'indi).cpp
+++ 008 (Minimum va maksimum yig'indi).cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+using namespace std;
+int main(){
+        long long s = 0, mn = 1e9, mx = 1, x;
+        for(int i = 0; i < 5; i ++){
+        cin  >> x;
+        s += x;
+        mn = min(mn, x);
+        mx = max(mx, x);
+}
+cout << s - mx << ' ' << s - mn;
+    return 0;
+}
