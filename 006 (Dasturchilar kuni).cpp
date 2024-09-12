53c85425e9299d722f5178f6a107cba467876b4e
Fazliddin <149813353+fazliddindev1@users.noreply.github.com>
2024-09-12 14:08:31 +0500

    Update 006 (Dasturchilar kuni).cpp

--- 006 (Dasturchilar kuni).cpp
+++ 006 (Dasturchilar kuni).cpp
@@ -1,17 +1,1 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
 
-int main(){
-    int y;
-    cin >> y;
-    if(y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)){
-        cout << "12/09/";
-    }
-    else
-    cout << "13/09/";
-    
-    cout << setfill('0')  << setw(4) << y;
-    
-  return 0;
-}
