222d8046bbac39bdfcf1d4df7c951a1ac9a837f1
Fazliddin <149813353+fazliddindev1@users.noreply.github.com>
2024-09-12 14:07:10 +0500

    Update 002(Katta-kichik).cpp

--- 002(Katta-kichik).cpp
+++ 002(Katta-kichik).cpp
@@ -1,16 +1,1 @@
-#include <iostream>
 
-using namespace std;
-
-int main(){
-    int A,B;
-    cin >> A >> B;
-    if (A == B){
-        cout << "=";
-    } else if(A < B){
-        cout << "<";
-    }else{
-        cout << ">";
-    }
-    return 0;
-}
