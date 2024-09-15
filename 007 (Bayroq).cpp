381ced25027a6a50e2ac88f7c27c0e77d1c8b4ff
Fazliddin <149813353+fazliddindev1@users.noreply.github.com>
2024-09-15 22:16:16 +0500

    Update 007 (Bayroq).cpp

--- 007 (Bayroq).cpp
+++ 007 (Bayroq).cpp
@@ -1,11 +1,1 @@
-#include <iostream>
-using namespace std;
 
-int main(){
-  int N;
-  cin >> N;
-  long long F[46] = {0, 1};
-  for(int i= 2; i <= 45; i++) F[i] = F[i-2] + F[i-1];
-  cout << F[N] * 2;
-  return 0;
-}
